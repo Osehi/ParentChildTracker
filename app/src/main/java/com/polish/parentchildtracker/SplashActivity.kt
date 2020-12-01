9f9a700b94cb8f7d530ceaa476ae1efa530f61ee
Osehi <osehiaseehilen@gmail.com>
2020-12-01 14:20:27 +0100

    'initial'

---
+++ app/src/main/java/com/polish/parentchildtracker/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.polish.parentchildtracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+}
