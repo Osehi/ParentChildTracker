63b36a2d0e83ff1595acfef1bafc17f861223f82
Osehi <osehiaseehilen@gmail.com>
2020-12-19 15:48:58 +0100

    viewpager adapter created

--- app/src/main/java/com/polish/parentchildtracker/adapter/OnBoardingViewPagerAdapter.kt
+++ app/src/main/java/com/polish/parentchildtracker/adapter/OnBoardingViewPagerAdapter.kt
@@ -1,4 +1,19 @@
 package com.polish.parentchildtracker.adapter
 
-class OnBoardingViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class OnBoardingViewPagerAdapter(list:ArrayList<Fragment>, fragmentManager:FragmentManager, lifecycle:Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    private val fragmentList:ArrayList<Fragment> = list
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
 }
