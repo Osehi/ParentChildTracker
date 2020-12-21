ee1e32505117922be8aaf91620f15a0f2f643671
Osehi <osehiaseehilen@gmail.com>
2020-12-21 14:33:33 +0100

    start button added

--- app/src/main/java/com/polish/parentchildtracker/view/MainActivity.kt
+++ app/src/main/java/com/polish/parentchildtracker/view/MainActivity.kt
@@ -3,10 +3,22 @@ package com.polish.parentchildtracker.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.polish.parentchildtracker.R
+import com.polish.parentchildtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * viewBinding reference
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /**
+         * initialize the binding object
+         */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
